fd2857cf56dd9ac2173e8ac695920b097d739579
Mathias Labora <mathiaslabora@gmail.com>
2022-02-20 14:39:06 -0300

    primer video finalizado

--- src/test/java/com/example/demo/EmployeeJpaRepositoryTest.java
+++ src/test/java/com/example/demo/EmployeeJpaRepositoryTest.java
@@ -1,8 +1,36 @@
 package com.example.demo;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.example.demo.model.Employee;
+import com.example.demo.repository.IEmployeeJpaRepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+
 @DataJpaTest
-@AutoConfigureTestDataBase(replace = Replace.NONE)
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class EmployeeJpaRepositoryTest {
+    @Autowired
+    private IEmployeeJpaRepository repo;
     
+@Test
+public void saveEmployee(){
+Employee jhon = new Employee("jhon", "smith", "empl123");
+Employee claire = new Employee("claire", "simpson", "empl12342");
+
+repo.save(jhon);
+repo.save(claire);
+
+repo.flush();
+
+assertEquals(2, repo.findAll().size());
+
+
+
+}
 }
